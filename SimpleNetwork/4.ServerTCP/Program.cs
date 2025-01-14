b7593b677e374839e77517dfcba3acf40ac9ac65
novakvova <novakvova@gmail>
2025-01-14 20:54:40 +0200

    Chat is good. Simple.

---
+++ SimpleNetwork/4.ServerTCP/Program.cs
@@ -0,0 +1,113 @@
+﻿using System.Net;
+using System.Net.Sockets;
+using System.Text;
+
+namespace _4.ServerTCP
+{
+    class Program
+    {
+        /// <summary>
+        /// Використаємо lock для коректної роботи сервера
+        /// </summary>
+        static readonly object _lock = new object();
+        /// <summary>
+        /// Зберігає набір клієнтів, які є в чаті
+        /// </summary>
+        static readonly Dictionary<int, TcpClient> list_clients = new Dictionary<int, TcpClient>();
+        static async Task Main(string []args)
+        {
+            //лічильнк клієнів у чаті
+            int countClient = 1;
+
+            Console.InputEncoding = Encoding.UTF8;
+            Console.OutputEncoding = Encoding.UTF8;
+            var hostName = Dns.GetHostName();
+            Console.WriteLine($"Мій хост {hostName}");
+            //Визначаю список IP адрес у моїй локальній мережі.
+            var localhost = await Dns.GetHostEntryAsync(hostName);
+
+            int count = localhost.AddressList.Length;
+            var ip = localhost.AddressList[count-1];
+            int i = 0;
+            Console.WriteLine("Вкажіть IP адресу:");
+            foreach (var item in localhost.AddressList)
+            {
+                Console.WriteLine($"{++i}.{item}");
+            }
+            Console.Write($"({ip})->_");
+            var temp = Console.ReadLine();
+            if(!string.IsNullOrEmpty(temp))
+                ip = IPAddress.Parse(temp);
+            int port = 4512;
+            Console.Title = $"Ваш IP {ip}:{port} :)";
+            TcpListener serverSocket = new TcpListener(ip, port);
+            serverSocket.Start();
+            Console.WriteLine($"Run Server {ip}:{port}");
+
+            while (true)
+            {
+                //Очікує клієнтів
+                TcpClient client = serverSocket.AcceptTcpClient();
+                lock(_lock)
+                {
+                    list_clients.Add(countClient, client);
+                }
+                Console.WriteLine($"На сервер додався клієнте {client.Client.RemoteEndPoint}");
+                Thread t = new Thread(handle_clients); //Запускає обробника запиітв у окремому потоці
+                t.Start(countClient); //
+                countClient++;
+            }
+        }
+        public static void handle_clients(object c)
+        {
+            int id = (int)c;
+            TcpClient client;
+            lock (_lock)
+            {
+                client = list_clients[id];
+            }
+            try
+            {
+                while (true)
+                {
+                    //Отримуємо потік від клієнта
+                    NetworkStream strm = client.GetStream();
+                    byte [] buffer = new byte[10240];
+                    int byte_count = strm.Read(buffer);
+                    if (byte_count==0)
+                        break;
+                    //Читаємо дані від клієнта
+                    string data = Encoding.UTF8.GetString(buffer, 0, byte_count);
+                    Console.WriteLine($"Client message {data}");
+                    brodcast(data);
+                }
+            }
+            catch { }
+
+            lock (_lock)
+            {
+                Console.WriteLine($"Чат покинув клієнт {client.Client.RemoteEndPoint}");
+                list_clients.Remove(id);
+            }
+            client.Client.Shutdown(SocketShutdown.Both);
+            client.Close();
+        }
+        //усім клієнта відправляємо мовідомлення
+        public static void brodcast(string data)
+        {
+            byte[] buffer = Encoding.UTF8.GetBytes(data);
+            lock (_lock)
+            {
+                try
+                {
+                    foreach(var c in list_clients.Values) //Отримали список клієнтів
+                    {
+                        NetworkStream stream = c.GetStream();
+                        stream.Write(buffer); //Кожному відправляємо повідомлення
+                    }
+                }
+                catch { }
+            }
+        }
+     }
+}
---
+++ SimpleNetwork/5.WPF_Client/MainWindow.xaml.cs
@@ -0,0 +1,184 @@
+﻿using System.IO;
+using System.Net;
+using System.Net.Http.Json;
+using System.Net.Sockets;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using Microsoft.Win32;
+using Newtonsoft.Json;
+
+namespace _5.WPF_Client
+{
+    public class UploadImage
+    {
+        public string Image { get; set; } = string.Empty;
+    }
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        private string _serverUrl = "https://kukumber.itstep.click";
+        private string _userImage = string.Empty;
+        private ChatMessage _message = new ChatMessage();
+        private TcpClient _tcpClient = new TcpClient();
+        private NetworkStream _ns; //посилання на потік
+        private Thread _thread;
+        //Початок роботи ініціалізація
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+
+        //Коли ми завершуємо роботу
+        private void Window_Closing(object sender, System.ComponentModel.CancelEventArgs e)
+        {
+            //Повідомлення надсилаю усім про завершення роботи в чаті
+            _message.Text = "Покинув чат";
+            var buffer = _message.Serialize();
+            _ns.Write(buffer);
+
+            _tcpClient.Client.Shutdown(SocketShutdown.Both);
+            _tcpClient.Close();
+        }
+
+        private void btnPhotoSelect_Click(object sender, RoutedEventArgs e)
+        {
+            OpenFileDialog dlg = new OpenFileDialog();
+            dlg.ShowDialog();
+            var filePath = dlg.FileName;
+            var bytes = File.ReadAllBytes(filePath);
+            var base64 = Convert.ToBase64String(bytes);
+            string json = JsonConvert.SerializeObject(new
+            {
+                photo = base64,
+            });
+            bytes = Encoding.UTF8.GetBytes(json);
+            WebRequest request = WebRequest.Create($"{_serverUrl}/api/galleries/upload");
+            request.Method = "POST";
+            request.ContentType = "application/json";
+            using (var stream = request.GetRequestStream())
+            {
+                stream.Write(bytes, 0, bytes.Length);
+            }
+            try
+            {
+                var response = request.GetResponse();
+                using (var stream = new StreamReader(response.GetResponseStream()))
+                {
+                    string data = stream.ReadToEnd();
+                    var resp = JsonConvert.DeserializeObject<UploadImage>(data);
+                    MessageBox.Show(_serverUrl + resp.Image);
+                    if (resp != null)
+                        _userImage = resp.Image;
+                }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+        }
+
+        private void ViewMessage(string text, string imageUrl)
+        {
+            var grid = new Grid();
+            for (int i = 0; i < 2; i++)
+            {
+                var colDef = new ColumnDefinition();
+                colDef.Width = GridLength.Auto;
+                grid.ColumnDefinitions.Add(colDef);
+            }
+            BitmapImage bmp = new BitmapImage(new Uri($"{_serverUrl}{imageUrl}"));
+            var image = new Image();
+            image.Source = bmp;
+            image.Width = 50;
+            image.Height = 50;
+
+            var textBlock = new TextBlock();
+            Grid.SetColumn(textBlock, 1);
+            textBlock.VerticalAlignment = VerticalAlignment.Center;
+            textBlock.Margin = new Thickness(5, 0, 0, 0);
+            textBlock.Text = text;
+            grid.Children.Add(image);
+            grid.Children.Add(textBlock);
+
+            lbInfo.Items.Add(grid);
+            lbInfo.Items.MoveCurrentToLast();
+            lbInfo.ScrollIntoView(lbInfo.Items.CurrentItem);
+        }
+
+        private void btnConnect_Click(object sender, RoutedEventArgs e)
+        {
+            if (string.IsNullOrEmpty(_userImage))
+            {
+                MessageBox.Show("Оберіть фото для корристувача");
+                return;
+            }
+            if (string.IsNullOrEmpty(txtUserName.Text))
+            {
+                MessageBox.Show("Вкажіть назву користувача");
+                return;
+            }
+            try
+            {
+                IPAddress ip = IPAddress.Parse("172.30.17.6");
+                //IPAddress ip = IPAddress.Parse("4.232.129.33");
+                int port = 4512;
+                _message.UserId = Guid.NewGuid().ToString();
+                _message.Name = txtUserName.Text;
+                _message.Photo = _userImage;
+                _tcpClient.Connect(ip, port);
+                _ns = _tcpClient.GetStream();
+                _thread = new Thread(obj => ResponseData((TcpClient)obj));
+                _thread.Start(_tcpClient);
+                btnSend.IsEnabled = true;
+                btnConnect.IsEnabled = false;
+                txtUserName.IsEnabled = false;
+                _message.Text = "Приєднався до чату";
+                var buffer = _message.Serialize();
+                _ns.Write(buffer); //Віпадправляю на сервер повідомлення про підключення
+
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("Проблема підключення до серверу " + ex.Message);
+            }
+        }
+        //Ловимо відповіді від сервера, коли він кидає brodcast
+        private void ResponseData(TcpClient client)
+        {
+            NetworkStream ns = client.GetStream();
+            byte[] readBytes = new byte[16054400];
+            int byte_count;
+            //читаємо відповідь від сервера, поки не буде 0
+            while ((byte_count = ns.Read(readBytes)) > 0)
+            {
+                Dispatcher.BeginInvoke(new Action(() =>
+                {
+                    ChatMessage msg = ChatMessage.Desserialize(readBytes);
+                    string text = $"{msg.Name} -> {msg.Text}";
+                    ViewMessage(text, msg.Photo);
+
+                }));
+            }
+        }
+
+        private void btnSend_Click(object sender, RoutedEventArgs e)
+        {
+            _message.Text = txtText.Text;
+            var buffer = _message.Serialize();
+            _ns.Write(buffer);
+
+            txtText.Text = "";
+        }
+    }
+}
