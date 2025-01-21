669e68cb7f6882c43590d9df2ebd8e191467f62e
novakvova <novakvova@gmail>
2025-01-21 20:24:42 +0200

    Send email message

---
+++ SimpleNetwork/7.SMTP/Message.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _7.SMTP
+{
+    public class Message
+    {
+        /// <summary>
+        /// Тема листа
+        /// </summary>
+        public string Subject { get; set; }
+        /// <summary>
+        /// Вміст листа
+        /// </summary>
+        public string Body { get; set; }
+        /// <summary>
+        /// Кому на пошту піде лист
+        /// </summary>
+        public string To { get; set; }
+    }
+}
---
+++ SimpleNetwork/7.SMTP/Program.cs
@@ -0,0 +1,47 @@
+﻿using MimeKit;
+using MailKit.Net.Smtp;
+
+namespace _7.SMTP
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            EmailConfiguration config = new EmailConfiguration();
+            Message message = new Message
+            {
+                Body = "Треба іти гуляти",
+                Subject = "Виходь на вулицю",
+                To = "novakvova@gmail.com"
+            };
+
+            // Створення повідомлення
+            var emailMessage = new MimeMessage();
+            emailMessage.From.Add(new MailboxAddress(config.From));
+            emailMessage.To.Add(new MailboxAddress(message.To));
+            emailMessage.Subject = message.Subject;
+
+            // Тіло повідомлення
+            emailMessage.Body = new TextPart("plain")
+            {
+                Text = message.Body
+            };
+
+            using var client = new SmtpClient();
+            try
+            {
+                client.Connect(config.SmtpServer, config.Port, true);
+                client.Authenticate(config.UserName, config.Password);
+                client.Send(emailMessage);
+                client.Disconnect(true);
+               
+                Console.WriteLine("Лист успішно відправлено!");
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
